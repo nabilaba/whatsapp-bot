26164898577ae040e4f072f17c712f746f02f8be
nabilaba <nabilazizbimaanggita@gmail.com>
2023-07-28 21:40:46 +0700

    added alternatif link download for ig

--- tugas/ig.js
+++ tugas/ig.js
@@ -1,4 +1,5 @@
 const { snapsave } = require("@bochilteam/scraper");
+const { igdl } = require("btch-downloader");
 
 const ig = async (msg, client) => {
   if (!msg.body.split(" ")[1].includes("instagram")) {
@@ -8,10 +9,16 @@ const ig = async (msg, client) => {
   client.sendMessage(msg.from, "Sedang di proses 😊...");
   try {
     let txt = "*Link Download*\n\n";
-    const link = await snapsave(msg.body.split(" ")[1]);
-    txt += link[0].url;
+
+    const btch = await igdl(msg.body.split(" ")[1]);
+    if (btch.url[0]) txt += btch.url[0];
+
+    const snap = await snapsave(msg.body.split(" ")[1]);
+    if (snap[0].url) txt += `\n\n${snap[0].url}`;
     client.sendMessage(msg.from, txt);
+
   } catch (error) {
+    console.log(error);
     client.sendMessage(
       msg.from,
       "Maaf, tidak dapat mendapatkan link download 😢"
